a8264452ed36934efd537900c4644041c95d5d6f
Azzam <samarafc46@gmail.com>
2024-01-16 07:49:38 +0700

    first commit

---
+++ telwin/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        xx : '#9ca3af',
+        cc : '#f1f5f9'
+      }
+    },
+  },
+  plugins: [],
+}
+
